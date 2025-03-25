8bf9525e4419b43bda192cfc649433096a293522
Jen <jensavino26@gmail.com>
2025-03-24 22:26:36 -0400

    created config file for render

---
+++ server/config/config.js
@@ -0,0 +1,25 @@
+// filepath: c:\Users\Albert\bootcamp\challenge-14-kanban\server\config\config.js
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+  },
+  scripts: {
+    migrate: "npx sequelize-cli db:migrate --config ./config/config.js",
+    seed: "node dist/seeds/index.js"
+  }
+};
